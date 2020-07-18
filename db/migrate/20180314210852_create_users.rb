7f788361406062a679fcdefced3d7b41b8990e87
marcusLau <lau.marcus76@gmail.com>
2020-07-18 15:33:55 -0700

    Done.

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def up
-  end
-  
-  def down
+  def change
+    create_table :users do |s|
+      s.string :username
+      s.string :password_digest
+    end
   end
 end
